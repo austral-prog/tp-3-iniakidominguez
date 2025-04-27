fb148669cc60c928c1917a6ccab360aa50458e14
iniakidominguez <73805462+iniakidominguez@users.noreply.github.com>
2025-04-27 14:09:57 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -10,3 +10,5 @@ def slice_simple():
     print(texto [0:3])
     print(texto [medio-1:medio+2])
     print(texto)
+    # Para verificar este ejercicio ejecutar el comando
+    # pytest tp3_slice_simple_test.py o python tp3_slice_simple_test.py 
